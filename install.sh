9325157611c8102bd08174550bb077af1e377250
AntonK <anton.kz.biz@gmail.com>
2020-08-09 08:28:31 +0300

    installation starting service and creating /etc/hs_hash_storage

--- install.sh
+++ install.sh
@@ -13,3 +13,15 @@ cp -f hash_shield_main /usr/local/bin/
 
 cp -f hash-shield-startup.service /etc/systemd/system/
 
+touch /etc/hs_hash_storage
+
+systemctl enable hash-shield-startup.service
+
+if [ "$?" -eq "0" ]; then
+	echo "enabled service successfully";
+else
+	echo "failed to enable service";
+	exit 1;
+fi
+
+echo "installation completed successfully";
--- uninstall.sh
+++ uninstall.sh
@@ -4,4 +4,6 @@ rm -f /usr/local/bin/hash_shield_main
 
 rm -f /etc/systemd/system/hash-shield-startup.service
 
+rm -f /etc/hs_hash_storage
+
 make clean
